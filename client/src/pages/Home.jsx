e7765b11d04da914e246b829452e2c8daa20cde6
PreeyanuchYu <babymoonshop@gmail.com>
2023-10-17 01:59:49 +0700

    Style: FAQ section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,11 +7,13 @@ import icon03 from "../assets/images/icon03.png";
 import featureImg from "../assets/images/feature-img.png";
 import videoIcon from "../assets/images/video-icon.png";
 import avatarIcon from "../assets/images/avatar-icon.png";
+import faqImg from "../assets/images/faq-img.png";
 import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs";
 import About from "../components/About/About";
 import ServiceList from "../components/Services/ServiceList";
 import DoctorList from "../components/Doctors/DoctorList";
+import FaqList from "../components/Faq/FaqList";
 
 const Home = () => {
   return (
@@ -263,6 +265,26 @@ const Home = () => {
         </div>
       </section>
       {/* our great doctors end */}
+
+      {/* FAQ section */}
+      <section>
+        <div className="container">
+          <div className="flex justify-between gap-[50px] lg:gap-0">
+            <div className="w-1/2 hidden md:block">
+              <img src={faqImg} alt="" />
+            </div>
+
+            <div className="w-full md:w-1/2">
+              <h2 className="heading">
+                Most questions by our beloved patients
+              </h2>
+              <FaqList />
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* FAQ section END */}
+
     </>
   );
 };
